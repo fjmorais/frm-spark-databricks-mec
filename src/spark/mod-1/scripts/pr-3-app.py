b9d80329e2ff05cbb1f4bb93898cfdd921295761
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2025-04-02 13:02:01 -0300

    update files

--- src/spark/mod-1/scripts/pr-3-app.py
+++ src/spark/mod-1/scripts/pr-3-app.py
@@ -1,10 +1,9 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
-    .appName("pr-3-app") \
     .getOrCreate()
 
-df_users = spark.read.json("data/users.json")
+df_users = spark.read.json("users.json")
 count = df_users.count()
 df_users.show(3)
 
