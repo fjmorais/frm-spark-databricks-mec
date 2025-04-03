66be4f9d6fb3b10e278b9c4ae23204ea4abe8295
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2025-04-03 08:21:48 -0300

    new lab spec

--- src/app/get-users-json.py
+++ src/app/get-users-json.py
@@ -1,3 +1,10 @@
+"""
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  /opt/bitnami/spark/jobs/app/get-users-json.py
+"""
+
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
